0a949f82c6cd8a5ef78712c29363b53eae0f02e6
nilufer polat <niluferpolat2@gmail.com>
2025-12-12 15:32:38 +0300

    changed url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "@/store/auth.store";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://corebanking-module-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
